007ace3956504c49b9f0b4e442d43c8691d51360
skynate22 <skynate22@naver.com>
2020-11-20 13:01:12 +0900

    bbs_book

---
+++ book/README.md
@@ -0,0 +1,9 @@
+# Spring-boot thymeleaf view\
+
+* HTML, CSS, JS가 적용된 Static web, HTML 파일에 Controller로 부터 전달되어 온 데이터를
+기본모양을 흐트리지않는 범위에서 자유롭게 표현하는 view
+
+* JSP와 비교했을때 JSP는 서버를 거쳐서 Rendering이 되지 않으면 어떻게 확면이 구성되는지
+그결과를 알기가 어렵다
+
+* 표준 web browser에서 
---
+++ book/src/main/java/com/biz/book/domain/BookVO.java
@@ -0,0 +1,39 @@
+package com.biz.book.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+@Table(name="tbl_book")
+public class BookVO {
+
+    @Id // 기본키 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // vo에서 저장되어있는 변수이름하고 테이블에서 저장되어있는  변수명을 다르게 쓰고싶을때 사용
+    @Column(name = "id", columnDefinition = "BIGINT")
+    private Long id;
+
+    @Column(name = "str_name")
+    private String strName;
+
+    @Column(columnDefinition = "VARCHAR(125)")
+    private String title;
+
+    @Column(length= 30)
+    private String author;
+
+    @Column(length = 125)
+    private String comp;
+    private String price;
+
+
+
+}
+
